ae9516ebd7b412ab39ddeeb8ac411ce7c6e25f1c
karthik singh <31167294+karthik-36@users.noreply.github.com>
2022-04-20 22:00:10 -0500

    menu done

--- app/src/main/java/com/example/forager/MainActivity.java
+++ app/src/main/java/com/example/forager/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.forager;
 
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -23,11 +25,15 @@ private ActivityMainBinding binding;
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications , R.id.navigation_settings)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications , R.id.navigation_settings)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        //getSupportActionBar().hide();
+
+
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
--- app/src/main/java/com/example/forager/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/forager/ui/home/HomeFragment.java
@@ -1,13 +1,18 @@
 package com.example.forager.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -19,6 +24,8 @@ public class HomeFragment extends Fragment {
 private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
 
         View view = inflater.inflate(R.layout.fragment_home , container , false);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
@@ -33,23 +40,22 @@ private FragmentHomeBinding binding;
         String[] menuItems = {"Milk" , "Eggs" , "Peanut butter" , "cottage cheese" , "bananas" , "apples" , "basmati rice" ,
                 "cashews"};
 
-        String[] donorName = {"karthik" , "aditya" , "peter" , "erick" , "Nate" , "paul" , "karthik" ,
-                "amrit"};
+        String[] donorName = {"karthik" , "aditya" , "peter" , "erick" , "Nate" , "paul" , "karthik" , "amrit"};
 
-        int[] images = {R.drawable.milk , R.drawable.eggs , R.drawable.pb , R.drawable.cottage_cheese , R.drawable.bananas , R.drawable.apple , R.drawable.basmati };
+        int[] images = {R.drawable.milk , R.drawable.eggs , R.drawable.pb , R.drawable.cottage_cheese , R.drawable.bananas , R.drawable.apple , R.drawable.basmati , R.drawable.cashew };
 
         ListView listview = (ListView) root.findViewById(R.id.mainMenu);
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.single_row,
-                R.id.itemName,
-                menuItems
-        );
+        kAdapter adapter = new kAdapter(getContext() , menuItems , images , donorName);
 
+//        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+//                getActivity(),
+//                R.layout.single_row,
+//                R.id.itemName,
+//                menuItems
+//        );
 
-        listview.setAdapter(listViewAdapter);
-
+        listview.setAdapter(adapter);
 
         return root;
     }
@@ -62,7 +68,45 @@ private FragmentHomeBinding binding;
 }
 
 
+class kAdapter extends ArrayAdapter<String>
+{
+    Context context;
+    int[] images;
 
+    String[] menuItems;
+
+    String[] donorName;
+
+    String[] expiryDate = {"2 days"};
+
+    kAdapter(Context c , String[] mI , int imgs[] , String[] dN ){
+        super(c, R.layout.single_row , R.id.itemName , mI );
+        this.context = c;
+        this.images = imgs;
+        this.menuItems = mI;
+        this.donorName = dN;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.single_row , parent , false);
+
+        ImageView myImage = (ImageView) row.findViewById(R.id.imageView);
+        TextView itemName = (TextView) row.findViewById(R.id.itemName);
+        TextView expiryText = (TextView) row.findViewById(R.id.expiryText);
+        TextView donorText = (TextView) row.findViewById(R.id.donorText);
+
+        myImage.setImageResource(images[position]);
+        itemName.setText(menuItems[position]);
+        expiryText.setText(expiryDate[0]);
+        donorText.setText(donorName[position]);
+
+        return row;
+    }
+}
 
 
 

52eb6d43e278f57d7a5c56ab1778108c4ef7e8a2
karthik singh <31167294+karthik-36@users.noreply.github.com>
2022-04-25 03:40:40 -0500

    before camera

--- app/src/main/java/com/example/forager/ui/confirm/ConfirmFragment.java
+++ app/src/main/java/com/example/forager/ui/confirm/ConfirmFragment.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.forager.R;
 import com.example.forager.databinding.FragmentConfirmBinding;
-import com.example.forager.ui.confirm.ConfirmViewModel;
 
 public class ConfirmFragment extends Fragment {
 
@@ -21,10 +21,14 @@ private FragmentConfirmBinding binding;
         ConfirmViewModel messagesViewModel =
                 new ViewModelProvider(this).get(ConfirmViewModel.class);
 
-        binding = FragmentConfirmBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = inflater.inflate(R.layout.fragment_confirm, container, false);
+//
+//        binding = FragmentConfirmBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
 
-        return root;
+
+
+        return view;
     }
 
 
